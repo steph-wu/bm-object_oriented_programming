fda2ad63472e1e7260358c3cfafd99e90dac81ce
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-18 15:14:49 -0500

    Create quota method

--- exercise4/paperboy.rb
+++ exercise4/paperboy.rb
@@ -3,10 +3,30 @@ class Paperboy
   attr_accessor :name, :experience
   attr_reader :earnings
 
-  def initialize(name, experience, earnings = 0)
+  def initialize(name, experience = 0, earnings = 0)
     @name = name
     @experience = experience
     @earnings = earnings
   end
 
+  def quota
+    quota = 50 + (@experience / 2)
+    @experience += quota
+    return quota
+  end
+
 end
+
+jim = Paperboy.new("Jim", 0)
+
+puts "week 1"
+puts jim.experience
+puts jim.quota
+
+puts "week 2"
+puts jim.experience
+puts jim.quota
+
+puts "week 3"
+puts jim.experience
+puts jim.quota
