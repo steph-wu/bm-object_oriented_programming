dffcdf67e31c2dbde34f6c3675259338075b2043
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-18 13:45:23 -0500

    Create accessor and deposit method

--- exercise1.rb
+++ exercise1.rb
@@ -1,8 +1,14 @@
 class BankAccount
 
+  attr_accessor :balance, :interest_rate
+
   def initialize(balance, interest_rate)
     @balance = balance
     @interest_rate = interest_rate
+  end
+
+  def deposit(amount)
+    @balance += amount
   end
 
 end
