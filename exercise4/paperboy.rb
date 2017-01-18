f1f923f98a8aa1e55d44ed741fb57a81c50cad3f
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-18 15:36:59 -0500

    Create deliver method which calculates earnings and updates experience

--- exercise4/paperboy.rb
+++ exercise4/paperboy.rb
@@ -10,23 +10,29 @@ class Paperboy
   end
 
   def quota
-    quota = 50 + (@experience / 2)
-    @experience += quota
-    return quota
+    quota = 50 + (@experience.to_i / 2)
+  end
+
+  def deliver(start_address, end_address)
+    if end_address - start_address >= quota
+      @earnings += quota * 0.25
+      @earnings += (end_address - start_address + 1 - quota) * 0.50
+    else
+      @earnings += (end_address - start_address + 1) * 0.25
+      @earnings -= 2
+    end
+    @experience += end_address - start_address + 1
+    return @earnings
   end
 
 end
 
 jim = Paperboy.new("Jim", 0)
 
-puts "week 1"
-puts jim.experience
 puts jim.quota
+puts jim.deliver(101,160)
+puts jim.earnings
 
-puts "week 2"
-puts jim.experience
 puts jim.quota
-
-puts "week 3"
-puts jim.experience
-puts jim.quota
+puts jim.deliver(1,75)
+puts jim.earnings
