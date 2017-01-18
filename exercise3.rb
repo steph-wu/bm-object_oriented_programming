3465a5b43cf820e23cb3d55894d5fa364f0895a4
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-18 14:37:08 -0500

    Create do battle and restart method

--- exercise3.rb
+++ exercise3.rb
@@ -1,5 +1,7 @@
 class Player
 
+  attr_accessor :gold_coins, :health_points, :lives, :score
+
   def initialize(gold_coins = 0, health_points = 10, lives = 5, score = 0)
     @gold_coins = gold_coins
     @lives = lives
@@ -7,4 +9,36 @@ class Player
     @score = score
   end
 
+  def restart
+    @gold_coins = 0
+    @health_points = 10
+    @lives = 5
+    @score = 0
+  end
+
+  def do_battle
+    if @health_points > 1
+      @health_points -= 1
+    elsif @lives == 0
+      restart
+    else
+      @health_points = 10
+      @lives -= 1
+    end
+  end
+
 end
+
+test = Player.new(10, 1, 2, 5)
+
+puts test.gold_coins
+puts test.health_points
+puts test.lives
+puts test.score
+
+test.do_battle
+
+puts test.gold_coins
+puts test.health_points
+puts test.lives
+puts test.score
