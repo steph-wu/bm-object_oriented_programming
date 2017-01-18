8738381de82c5b8a68a7814a3bb452627b3618ee
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-18 14:08:09 -0500

    Create two instances of cat and  method to display meal time

--- exercise2.rb
+++ exercise2.rb
@@ -8,4 +8,21 @@ class Cat
     @meal_time = meal_time
   end
 
+  def eats_at
+    case
+    when @meal_time == 0
+      return "12 AM"
+    when @meal_time > 0 && @meal_time < 12
+      return "#{@meal_time} AM"
+    when @meal_time == 12
+      return "12 PM"
+    when @meal_time > 12
+      return "#{@meal_time - 12} PM"
+    end
+  end
+
 end
+
+
+bean = Cat.new('Bean', 'kibble', 18)
+pumpkin = Cat.new('Pumpkin', 'milk', 11)
