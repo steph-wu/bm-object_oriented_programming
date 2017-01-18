7ce87cf4416dc928fb4ac52bc362517c96ec166a
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-18 14:19:10 -0500

    Create player class

---
+++ exercise3.rb
@@ -0,0 +1,10 @@
+class Player
+
+  def initialize(gold_coins = 0, health_points = 10, lives = 5, score = 0)
+    @gold_coins = gold_coins
+    @lives = lives
+    @health_points = health_points
+    @score = score
+  end
+
+end
